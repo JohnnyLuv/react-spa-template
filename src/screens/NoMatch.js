f126727fd0ae96ebb695c9f094103a7e890d6624
Johnny <949394771@qq.com>
2020-11-16 16:20:40 +0800

    [feat] 404 page

--- src/screens/NoMatch.js
+++ src/screens/NoMatch.js
@@ -1,11 +1,16 @@
 import { Link } from 'react-router-dom'
 
+import { Result, Button } from 'antd'
+
 
 function Main() {
   return (
-    <div style={{ marginTop: '10vh', textAlign: 'center' }}>
-      <h3>404 NotFound, go <Link to='/'>home page</Link></h3>
-    </div>
+    <Result
+      status='404'
+      title='404'
+      subTitle='Sorry, the page you visited does not exist.'
+      extra={<Link to='/'><Button type='primary'>Back Home</Button></Link>}
+    />
   )
 }
 
