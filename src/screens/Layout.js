60ac8a638065394381ba8c2d749626105218aefb
Johnny <949394771@qq.com>
2020-11-16 13:56:14 +0800

    initial layout

---
+++ src/screens/Layout.js
@@ -0,0 +1,80 @@
+import '../assets/style/layout.sass'
+
+import { Layout, Menu, Breadcrumb, Badge, Avatar, Button, Dropdown } from 'antd'
+import {
+  DatabaseOutlined, DeploymentUnitOutlined, TeamOutlined, BuildOutlined,
+  MessageOutlined, SettingOutlined, LogoutOutlined,
+} from '@ant-design/icons'
+const { Header, Content, Footer } = Layout
+const { SubMenu } = Menu
+
+function MyLayout() {
+  return (
+    <Layout>
+      <Header id='my-header' mode='horizontal'>
+        <div className="logo" >MIC OA</div>
+        <Menu className='sider-menu' mode="horizontal" defaultSelectedKeys={['11']}>
+          <SubMenu key="1" icon={<DatabaseOutlined />} title='项目管理'>
+            <Menu.Item key="11">Option 1</Menu.Item>
+            <Menu.Item key="12">Option 2</Menu.Item>
+          </SubMenu>
+          <SubMenu key="2" icon={<DeploymentUnitOutlined />} title='内部管理'>
+            <Menu.Item key="21">Option 1</Menu.Item>
+            <Menu.Item key="22">Option 2</Menu.Item>
+          </SubMenu>
+          <SubMenu key="3" icon={<TeamOutlined />} title='人员管理'>
+            <Menu.Item key="31">Option 1</Menu.Item>
+            <Menu.Item key="32">Option 2</Menu.Item>
+          </SubMenu>
+          <SubMenu key="4" icon={<BuildOutlined />} title='模板'>
+            <Menu.Item key="41">Option 1</Menu.Item>
+            <Menu.Item key="42">Option 2</Menu.Item>
+          </SubMenu>
+        </Menu>
+        <Dropdown overlay={<Menu>
+          <Menu.Item icon={<MessageOutlined />}>消息</Menu.Item>
+          <Menu.Item icon={<SettingOutlined />}>设置</Menu.Item>
+          <Menu.Item icon={<LogoutOutlined />}>登出</Menu.Item>
+        </Menu>}>
+          <div>
+            <Badge count={7}>
+              <Avatar style={{ color: '#f56a00', backgroundColor: '#fde3cf', }}>J</Avatar>
+            </Badge>
+            <Button type='link' size='small'>Johnny</Button>
+          </div>
+        </Dropdown>
+      </Header>
+      <Content id='my-content'>
+        <Breadcrumb className='my-breadcrumb'>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <div className='my-body'>
+          <h1>Content</h1>
+          <h1>Content</h1>
+          <h1>Content</h1>
+          <h1>Content</h1>
+          <h1>Content</h1>
+          <h1>Content</h1>
+          <h1>Content</h1>
+          <h1>Content</h1>
+          <h1>Content</h1>
+          <h1>Content</h1>
+          <h1>Content</h1>
+          <h1>Content</h1>
+          <h1>Content</h1>
+          <h1>Content</h1>
+          <h1>Content</h1>
+          <h1>Content</h1>
+          <h1>Content</h1>
+          <h1>Content</h1>
+          <h1>Content</h1>
+        </div>
+      </Content>
+      <Footer id='my-footer'>2020 © MicMua TECH</Footer>
+    </Layout>
+  )
+}
+
+export default MyLayout
