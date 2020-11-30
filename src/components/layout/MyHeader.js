8269a29ddcc0adc23c3e065c46ed0259cff02e89
Johnny <949394771@qq.com>
2020-11-30 10:21:09 +0800

    [feat] 项目列表

--- src/components/layout/MyHeader.js
+++ src/components/layout/MyHeader.js
@@ -1,42 +1,114 @@
 import { useHistory } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 import { Layout, Menu, Badge, Avatar, Button, Dropdown } from 'antd'
 import {
-  DatabaseOutlined, DeploymentUnitOutlined, TeamOutlined, BuildOutlined,
+  HomeOutlined, DatabaseOutlined, DeploymentUnitOutlined, TeamOutlined, BuildOutlined,
   MessageOutlined, SettingOutlined, LogoutOutlined,
 } from '@ant-design/icons'
 const { Header } = Layout
 const { SubMenu } = Menu
 
 
+/**
+ * 组件 layout header
+ */
 function Main() {
   const history = useHistory()
+
+  const menuList = [
+    {
+      title: '首页',
+      path: '/',
+      icon: <HomeOutlined />,
+      children: []
+    },
+    {
+      title: '项目管理',
+      path: '/project-management',
+      icon: <DatabaseOutlined />,
+      children: [
+        {
+          title: '项目列表',
+          path: '/project-management/list',
+          icon: '',
+          children: []
+        }
+      ]
+    },
+    {
+      title: '内部管理',
+      path: '',
+      icon: <DeploymentUnitOutlined />,
+      children: [
+        {
+          title: 'menu1',
+          path: '/inner-management/menu1',
+          icon: '',
+          children: []
+        },
+        {
+          title: 'menu2',
+          path: '/inner-management/menu2',
+          icon: '',
+          children: []
+        }
+      ]
+    },
+    {
+      title: '人员管理',
+      path: '',
+      icon: <TeamOutlined />,
+      children: [
+        {
+          title: 'menu1',
+          path: '/personnel-management/menu1',
+          icon: '',
+          children: []
+        },
+        {
+          title: 'menu2',
+          path: '/personnel-management/menu2',
+          icon: '',
+          children: []
+        }
+      ]
+    },
+    {
+      title: '模板',
+      path: '/template-management',
+      icon: <BuildOutlined />,
+    },
+  ]
 
   function signOut() {
     window.localStorage.clear()
     history.push('/signin')
   }
 
+  function onMenuChanged({ item }) {
+    const _title = item.props.children[1]
+    const _path = item.props.eventKey
+    console.log(_title, _path)
+    history.push({
+      pathname: _path,
+      state: {
+        title: _title,
+        path: _path,
+      },
+    })
+  }
+
   return (
     <Header id='my-header' mode='horizontal'>
-      <div className="logo" >MIC OA</div>
-      <Menu className='sider-menu' mode="horizontal" defaultSelectedKeys={['11']}>
-        <SubMenu key="1" icon={<DatabaseOutlined />} title='项目管理'>
-          <Menu.Item key="11">项目列表</Menu.Item>
-          <Menu.Item key="12">Option 2</Menu.Item>
-        </SubMenu>
-        <SubMenu key="2" icon={<DeploymentUnitOutlined />} title='内部管理'>
-          <Menu.Item key="21">Option 1</Menu.Item>
-          <Menu.Item key="22">Option 2</Menu.Item>
-        </SubMenu>
-        <SubMenu key="3" icon={<TeamOutlined />} title='人员管理'>
-          <Menu.Item key="31">Option 1</Menu.Item>
-          <Menu.Item key="32">Option 2</Menu.Item>
-        </SubMenu>
-        <SubMenu key="4" icon={<BuildOutlined />} title='模板'>
-          <Menu.Item key="41">Option 1</Menu.Item>
-          <Menu.Item key="42">Option 2</Menu.Item>
-        </SubMenu>
+      <Link className='logo' to='/'>MIC OA</Link>
+      <Menu className='sider-menu' mode='horizontal' onSelect={onMenuChanged}>
+        {menuList.map(menu => menu.children?.length > 0
+          ? <SubMenu key={menu.path} icon={menu.icon} title={menu.title}>
+            {menu.children.map(child => <Menu.Item key={child.path}>{child.title}</Menu.Item>)}
+          </SubMenu>
+          : <Menu.Item key={menu.path}>{menu.title}</Menu.Item>
+        )}
       </Menu>
       <Dropdown overlay={<Menu>
         <Menu.Item icon={<MessageOutlined />}>消息</Menu.Item>
@@ -50,7 +122,7 @@ function Main() {
           <Button type='link' size='small'>{window.localStorage.getItem('username')}</Button>
         </div>
       </Dropdown>
-    </Header>
+    </Header >
   )
 }
 
